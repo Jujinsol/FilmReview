9266bdea252b0be7f605b00505e7d4dbdb1edca9
주진성 <jjsair0412@naver.com>
2021-08-23 20:48:47 +0900

    회원가입 서버 구현

--- review/src/main/java/movieReview/review/controller/JoinController.java
+++ review/src/main/java/movieReview/review/controller/JoinController.java
@@ -1,15 +1,33 @@
 package movieReview.review.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import movieReview.review.dto.userInfo;
+import movieReview.review.repository.joinRepositoryImpl;
+import movieReview.review.service.joinServiceImpl;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.thymeleaf.exceptions.ParserInitializationException;
 
 @Controller
-@RequestMapping("")
 @Slf4j
+@RequiredArgsConstructor
 public class JoinController {
+    private final joinServiceImpl joinService;
 
+    @GetMapping("/Join")
+    public String joinPage(Model model){
+        model.addAttribute("userInfo",new userInfo());
+        return "Join/signUp";
+    }
 
-
-
+    @ResponseBody
+    @PostMapping("/Join")
+    public String joinUser(@ModelAttribute userInfo userinfo, RedirectAttributes redirectAttributes){
+        int join = joinService.join(userinfo.getEmail(), userinfo.getId(), userinfo.getPassword(), null);
+        String join2 = Integer.toString(join);
+        return join2;
+    }
 }
