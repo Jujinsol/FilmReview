4dc8e8ab1850cd567d1f2c7184af24279b95a021
주진성 <jjsair0412@naver.com>
2021-08-25 23:13:21 +0900

    로그인 기능 구현 완료 - 예외처리중 -2

--- review/src/main/java/movieReview/review/controller/LoginPage/LoginController.java
+++ review/src/main/java/movieReview/review/controller/LoginPage/LoginController.java
@@ -42,36 +42,47 @@ public class LoginController {
         //아예 존재하지않는지
         //사용자인지
         //관리자인지 구분해야됌
-        int result = loginServiceImpl.FirstCheck(userinfo.getId());
-        if(result==1){
-            //사용자
-            Optional<userInfo> userLoginResult = Optional.ofNullable(loginServiceImpl.userLogin(userinfo));
-            if(userLoginResult.isEmpty()){
-                bindingResult.addError(new FieldError("userinfo","id",userinfo.getId(),false,null,null,"잘못입력하셨습니다."));
-                return "redirect:/Login";
-            }else{
-                Cookie userCookie = new Cookie("id",String.valueOf(userinfo.getId()));
-                response.addCookie(userCookie);
-                return "/MainPage/MainPage";
-            }
-        }else if(result==2){
-            //관리자
-            mangerinfo.setId(userinfo.getId());
-            mangerinfo.setPassword(userinfo.getPassword());
-            mangerinfo.setEmail(userinfo.getEmail());
-            Optional<mangerInfo> mangerLoginResult = Optional.ofNullable(loginServiceImpl.mangerLogin(mangerinfo));
-            if(mangerLoginResult.isEmpty()){
-                bindingResult.addError(new FieldError("userinfo","id",userinfo.getId(),false,null,null,"잘못입력하셨습니다."));
-                return "redirect:/Login";
-            }else{
-                Cookie mangerCookie = new Cookie("id",String.valueOf(mangerinfo.getId()));
-                response.addCookie(mangerCookie);
-                return "/MainPage/MainPage";
-            }
-        }else{
-            //없는 회원 id
-            bindingResult.addError(new FieldError("userinfo","id",userinfo.getId(),false,null,null,"없는 회원입니다.회원가입을 진행해 주세요."));
+        if(userinfo.getPassword()==null){
+            bindingResult.addError(new FieldError("userinfo","password",userinfo.getId(),false,null,null,"잘못입력하셨습니다."));
             return "redirect:/Login";
+        }else if(userinfo.getId()==null){
+            bindingResult.addError(new FieldError("userinfo","ID",userinfo.getId(),false,null,null,"잘못입력하셨습니다."));
+            return "redirect:/Login";
+        }else if(userinfo.getPassword()==null&&userinfo.getId()==null){
+            bindingResult.addError(new FieldError("userinfo","ID",userinfo.getId(),false,null,null,"잘못입력하셨습니다."));
+            return "redirect:/Login";
+        }else{
+            int result = loginServiceImpl.FirstCheck(userinfo.getId());
+            if(result==1){
+                //사용자
+                Optional<userInfo> userLoginResult = Optional.ofNullable(loginServiceImpl.userLogin(userinfo));
+                if(userLoginResult.isEmpty()){
+                    bindingResult.addError(new FieldError("userinfo","id",userinfo.getId(),false,null,null,"잘못입력하셨습니다."));
+                    return "redirect:/Login";
+                }else{
+                    Cookie userCookie = new Cookie("id",String.valueOf(userinfo.getId()));
+                    response.addCookie(userCookie);
+                    return "/MainPage/MainPage";
+                }
+            }else if(result==2){
+                //관리자
+                mangerinfo.setId(userinfo.getId());
+                mangerinfo.setPassword(userinfo.getPassword());
+                mangerinfo.setEmail(userinfo.getEmail());
+                Optional<mangerInfo> mangerLoginResult = Optional.ofNullable(loginServiceImpl.mangerLogin(mangerinfo));
+                if(mangerLoginResult.isEmpty()){
+                    bindingResult.addError(new FieldError("userinfo","id",userinfo.getId(),false,null,null,"잘못입력하셨습니다."));
+                    return "redirect:/Login";
+                }else{
+                    Cookie mangerCookie = new Cookie("id",String.valueOf(mangerinfo.getId()));
+                    response.addCookie(mangerCookie);
+                    return "/MainPage/MainPage";
+                }
+            }else{
+                //없는 회원 id
+                bindingResult.addError(new FieldError("userinfo","id",userinfo.getId(),false,null,null,"없는 회원입니다.회원가입을 진행해 주세요."));
+                return "redirect:/Login";
+            }
         }
     }
 }
