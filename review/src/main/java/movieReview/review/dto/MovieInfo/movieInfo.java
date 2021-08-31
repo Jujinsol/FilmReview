f79fdab05f418a8c100d1af654e7f72ded31b77f
주진성 <jjsair0412@naver.com>
2021-08-31 22:36:25 +0900

    검증수정중

--- review/src/main/java/movieReview/review/dto/MovieInfo/movieInfo.java
+++ review/src/main/java/movieReview/review/dto/MovieInfo/movieInfo.java
@@ -4,12 +4,24 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class movieInfo {
+    @NotBlank
     private String movieName;
+
     private MultipartFile moviePoster;
+
+    @NotNull
     private int openYear;
+
+    @NotBlank
     private String directorName;
+    @NotBlank
     private String storyLine;
 }
