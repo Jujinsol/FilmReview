68fcbbfca61265d1ce6de7bf61833956fe2c1bb9
Jujinsol <88575150+Jujinsol@users.noreply.github.com>
2021-09-23 22:41:07 +0900

    html consolidation

--- review/src/main/java/movieReview/review/controller/MainPage/MainPageController.java
+++ review/src/main/java/movieReview/review/controller/MainPage/MainPageController.java
@@ -3,6 +3,7 @@ package movieReview.review.controller.MainPage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import movieReview.review.Domain.MovieInfo.JpaMovieInfo;
+import movieReview.review.Domain.MovieInfo.movieInfo;
 import movieReview.review.repository.MainPage.MainPageRepository;
 import movieReview.review.service.MainPage.MainPageService;
 import org.dom4j.rule.Mode;
@@ -23,7 +24,7 @@ public class MainPageController{
 
     @GetMapping
     // uri경로로 넘어오는 페이지번호를 받아서 출력
-    public String goMainPage(Model model ,@RequestParam(required = false, defaultValue = "0", value = "page") int page){
+    public String goMainPage(Model model ,@RequestParam(required = false, defaultValue = "0", value = "page") int page, @ModelAttribute("movieInfo") movieInfo movieInfo){
         Page<JpaMovieInfo> movieList = pageService.findAll(page); // 페이지 가져오기
         model.addAttribute("boardList",movieList);
         model.addAttribute("totalPage",movieList.getTotalPages()); // 전체페이지 모델에담아 전송
