d1d6f0bd6185a8c1b298fa502c3ea54e0dae4255
주진성 <jjsair0412@naver.com>
2021-10-01 01:40:42 +0900

    버그수정

--- review/src/main/java/movieReview/review/Domain/updatePageDto/updateDto.java
+++ review/src/main/java/movieReview/review/Domain/updatePageDto/updateDto.java
@@ -2,8 +2,13 @@ package movieReview.review.Domain.updatePageDto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class updateDto {
     private String id;
+
+    @NotNull(message = "필수로 작성해야 합니다.")
     private int password;
 }
