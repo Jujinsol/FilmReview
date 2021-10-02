92de525c98878676db4c0b5c131c086072297009
주진성 <jjsair0412@naver.com>
2021-10-03 01:08:23 +0900

    사진파일과 서버저장 사진파일 uuid로 저장

--- review/src/main/java/movieReview/review/controller/managerUpload/UploadController.java
+++ review/src/main/java/movieReview/review/controller/managerUpload/UploadController.java
@@ -6,6 +6,7 @@ import movieReview.review.Session.SessionConst;
 import movieReview.review.Domain.MovieInfo.movieInfo;
 import movieReview.review.service.Login.LoginService;
 import movieReview.review.service.Upload.UploadServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/Upload")
@@ -24,10 +26,11 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @PropertySource("classpath:application.properties")
 public class UploadController {
-    private File files;
     private final LoginService loginServiceImpl;
-    private final Environment environment;
     private final UploadServiceImpl uploadService;
+
+    @Value("${file.dir}")
+    private String fileDir;
 
     @GetMapping
     public String uploadPage(@SessionAttribute(value = SessionConst.LoginId, required = false) String id,Model model){
@@ -53,14 +56,13 @@ public class UploadController {
         }
 
         moviePoster = movieinfo.getMoviePoster();
-        files= new File(environment.getProperty("FilePath")+moviePoster.getOriginalFilename());
+        String storeFileName = createStoreFileName(moviePoster.getOriginalFilename());
 
-        // DB에 저장될 파일 경로
-        String path = files.getPath();
-        moviePoster.transferTo(files);
+        // 경로에 파일 저장
+        moviePoster.transferTo(new File(getFullPath(storeFileName)));
 
-        movieinfo.setPhotoOriName(moviePoster.getOriginalFilename());
-        movieinfo.setPhotoUri(path);
+        movieinfo.setPhotoOriName(storeFileName); //UUID로 변환된 사진파일이름 DB에 저장됨
+        movieinfo.setPhotoUri(fileDir+moviePoster.getOriginalFilename());
 
         // db 저장
         uploadService.create(movieinfo);
@@ -72,4 +74,18 @@ public class UploadController {
         return "/MyPage/managerUploadSuccess";
     }
 
+    private String createStoreFileName(String originalFilename) {
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext; // 서버에 저장할 파일명 생성
+    }
+
+    private String extractExt(String originalFileName){
+        int pos = originalFileName.lastIndexOf(".");
+        return originalFileName.substring(pos+1); // .을 기준으로 파일확장자명만 반환
+    }
+
+    public String getFullPath(String filename){
+        return fileDir+filename; // 파일 저장경로를 반환해준다. 파일저장폴더 + 원본파일명
+    }
 }
