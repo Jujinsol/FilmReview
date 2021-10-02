3b18149337144ce2a73ded6a5eb3210a870cab67
주진성 <jjsair0412@naver.com>
2021-10-03 00:36:30 +0900

    리뷰삭제 js의 분리

--- review/src/main/resources/static/js/Review/ReviewFunc.js
+++ review/src/main/resources/static/js/Review/ReviewFunc.js
@@ -21,3 +21,51 @@ $('#reviewUpload').click(function () {
     });
 });
 
+// 비어있는지 확인
+const isEmpty = function (value) {
+    if (value=="null" || value == "" || value == null || value == undefined || (value != null && typeof value == "object" && !Object.keys(value).length)) {
+        return true
+    } else {
+        return false
+    }
+};
+
+
+if (getReview.length === 1) {
+    $("#reviewsInfo").append('<div>등록된 리뷰가 없습니다</div>')
+} else {
+    $("#reviewsInfo").empty();
+    for (let i = 1; i < getReview.length; i++) {
+        $("#reviewsInfo").append('리뷰단사람 id : <div>' + getReview[i].reviewUser + '</div>')
+        $("#reviewsInfo").append('영화 점수 : <div>' + getReview[i].moviePoint + '</div>')
+        $("#reviewsInfo").append('리뷰 본문 : <div>' + getReview[i].movieReivew + '</div>')
+        $("#reviewsInfo").append('<button onclick="deleteReview(\'' + sessionId + '\');" >리뷰 삭제하기</button>')
+    }
+}
+
+window.deleteReview = function (reviewUser) {
+    if (isEmpty(reviewUser)) {
+        alert("로그인부터 진행해 주세요.")
+    } else {
+        if (confirm("리뷰를 삭제하시겠습니까?") == true) {
+            $.ajax({
+                type: "GET",
+                url: "/EachMovie/reviewDelete",
+                data: reviewUser,
+                success: function (deleteResult) {
+                    if (deleteResult == 0) {
+                        alert("로그인부터 진행해 주세요.")
+                    } else {
+                        alert("리뷰가 삭제되었습니다.")
+                        window.location.reload();
+                    }
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    alert("통신 실패.")
+                }
+            });
+        } else {
+            return;
+        }
+    }
+};
