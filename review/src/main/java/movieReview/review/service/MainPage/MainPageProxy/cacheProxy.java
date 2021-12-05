9ab67b9346cff2a729c22bc70f8e449571e25921
주진성 <jjsair0412@naver.com>
2021-12-05 15:07:09 +0900

    관리자가 영화등록하면 캐시값 초기화되게끔 리펙토링

--- review/src/main/java/movieReview/review/service/MainPage/MainPageProxy/cacheProxy.java
+++ review/src/main/java/movieReview/review/service/MainPage/MainPageProxy/cacheProxy.java
@@ -5,9 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import movieReview.review.Domain.MovieInfo.JpaMovieInfo;
 import movieReview.review.repository.MainPage.MainPageRepository;
 import movieReview.review.service.MainPage.MainPageService;
+import movieReview.review.service.RedisService.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -17,6 +22,7 @@ public class cacheProxy implements MainPageService {
     private final MainPageRepository targetRepository;
     private final ThreadLocal<Page> cacheValue = new ThreadLocal<>();
 
+
     @Override
     public Page<JpaMovieInfo> findAll(int page) {
 
@@ -24,11 +30,12 @@ public class cacheProxy implements MainPageService {
             log.info("프록시 캐시에 값 없음");
             PageRequest paging = PageRequest.of(page, 20);
             cacheValue.set(targetRepository.findAll(paging));
-        }else if(false){
-            // 관리자가 업로드했을때 동시에 cacheValue 초기화시켜줘야됨.
         }
-
         return cacheValue.get();
+    }
 
+    public void cacheReset(){
+        log.info("캐시값 초기화");
+        cacheValue.set(null);
     }
 }
