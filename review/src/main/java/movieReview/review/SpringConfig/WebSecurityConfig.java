03da437ce3da1ec0db9ec2ebc79dfc9c373011dc
주진성 <jjsair0412@naver.com>
2021-12-13 02:05:29 +0900

    스프링 시큐리티 설정파일 build.gradle에 설치

---
+++ review/src/main/java/movieReview/review/SpringConfig/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package movieReview.review.SpringConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
